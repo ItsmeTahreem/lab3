2a5ccb41d9055bb8a0fc9a25cfed59b8e1e6d7ab
TAHREEM SHAKEEL - STUDENT <G00347599@gmit.ie>
2019-09-30 11:48:27 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
-
+import {MovieServiceService} from './service/movie-service.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'movie-app';
+ // title = 'movie-app';
+  Mymovies:any = [];
+
+  constructor(private movieService: MovieServiceService){
+    this.movieService.GetMovieInformation().subscribe((data)=>{
+      this.Mymovies = data.Search;
+      console.log(this.Mymovies);
+
+      })
+  }
+
 }
